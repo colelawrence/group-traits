9533634795791834630f51ef48c6c56f0c361059
James Hibben <jmhibben@gmail.com>
2016-11-29 09:55:00 -0600

    Modifying CreateGroupsTest to morph the files

--- test/CreateGroupsTest.ts
+++ test/CreateGroupsTest.ts
@@ -1,3 +1,4 @@
+import * as assert from 'assert'
 
 import { Person, Trait } from '../src/models'
 import { GroupOrganizer } from '../src/group-organizer'
@@ -8,11 +9,16 @@ import { TestWorld, TestCase, printTestCase } from './TestWorld'
 import { diffGroupResults } from './helpers'
 import { readdirSync } from 'fs'
 
+import * as Morphs from './morph'
+
 describe('createGroups', () => {
 
 	const defaultGroupMin = 3
 	const defaultGroupMax = 7
     const casesdir = './test/cases/'
+
+    Morphs.MorphStorage.setCasesDir(casesdir)
+    Morphs.MorphStorage.getTestFiles()
 
 	var cases: { testcase: TestCase, filename: string }[]
 	= readdirSync(casesdir)
@@ -28,6 +34,11 @@ describe('createGroups', () => {
             .forEach(({ testcase, filename }) => {
                 it(`should match oracle in ${ filename }`, () => {
                     // printTestCase(testcase)
+
+                    let file: Morphs.File = Morphs.MorphStorage.getOriginalTestCase(filename)
+
+                    // modify the test case
+
 
                     const peoples = testcase.people
                     const traits = testcase.traits
@@ -46,31 +57,44 @@ describe('createGroups', () => {
     })
 
     describe('Adding an unused trait to a single person', () => {
+
         cases
             .forEach(({ testcase, filename }) => {
+                // get a copy of the Morphs.File object of the original test case
+                let file: Morphs.File = Morphs.parseFile(casesdir + filename)
+                // make sure the file gets stored
+                Morphs.MorphStorage.store(file)
+
                 it(`should not change the output in ${filename}`, () => {
+                    // get the last trait
+                    let keys = Object.keys(file.usedtraits).sort()
+                    // console.log(keys.length)
+                    let lastTrait = keys[keys.length - 1]
+                    // console.log(lastTrait)
+                    // set the primary data as the first unused trait
+                    let morph: Morphs.TData = {
+                        primary: [(parseInt(lastTrait) + 1).toString()]
+                    }
+                    // console.log('Morph:', morph)
+                    // morph the test data
+                    let morphed: Morphs.FileContents =
+                            Morphs.MorphStorage.morphTestFile(file, morph, file.parsedcontents, 1, Morphs.addTrait)
+
+                    morphed.description += "\nOne trait added to one person"
+
+                    file.parsedcontents = morphed
+                    // console.log(file.parsedcontents.people)
+
+                    let morphedfile = Morphs.MorphStorage.fileToString(file)
+
+                    console.log(morphedfile)
+                    let morphed_test: TestWorld =
+                            TestWorld.fromString(morphedfile)
+
                     const peoples = testcase.people
                     const traits = testcase.traits
 
-                    let useTrait: string
-
-                    for (let i = 0; i < 10; i++) {
-                        if (traits[i] === null || traits[i] === undefined) {
-                            useTrait = `${i+1}`
-                            break
-                        }
-                    }
-
-                    // add a trait to one of the people
-                    let person = peoples[0]
-                    let newPeoples: Person[] = []
-                    newPeoples.push(person)
-                    for (let i = 1; i < peoples.length; i++) {
-                        let p: Person = peoples[i]
-                        newPeoples.push(p)
-                    }
-
-                    const organizer = new GroupOrganizer(newPeoples, traits, [testcase.options.groupMin || defaultGroupMin, testcase.options.groupMax || defaultGroupMax])
+                    const organizer = new GroupOrganizer(peoples, traits, [testcase.options.groupMin || defaultGroupMin, testcase.options.groupMax || defaultGroupMax])
                     const results = organizer.getResults()
 
                     const diff: string = diffGroupResults(testcase.groups, results)
