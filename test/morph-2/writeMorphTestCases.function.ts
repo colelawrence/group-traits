d09001f8eccce18e88cb161aeb508bf1b4a02e04
James Hibben <jmhibben@gmail.com>
2016-12-14 23:25:11 -0600

    modifed the filename and prepped for morphing

--- test/morph-2/writeMorphTestCases.function.ts
+++ test/morph-2/writeMorphTestCases.function.ts
@@ -11,12 +11,25 @@ function writeMorphTestCases(
 	cases.forEach(tc => {
 		// TODO send the object to a transformer
 		// which recieves multiple labeled TestCaseObjects
-		// createMorphs(tco) => { morphdesc: string, morphedobject: TestCaseObject }
+		// createMorphs(tco) => { morphdesc: string, morphedobject: TestCaseObject }[]
+
+		// let morphed: { morphdesc: string, morphedobject: TCO.TestCaseObject }[] = createMorphs(tc.object) 
 
 		// then loop through those prefixing the path with it
 		fs.writeFileSync(
-			path.resolve(directory_out, 'm0-' + tc.filename),
+			path.resolve(directory_out, tc.filename),
+			// tc.object will be replaced by morphed.morphedobject
 			TCO.toString(tc.object)
 		)
     })
+}
+
+/**
+ * This will eventually call the morphing functions on each file to prepare for writing out
+ */
+function createMorphs(tco: TCO.TestCaseObject): {morphdesc: string, morphedobject: TCO.TestCaseObject}[] {
+	let morph_desc: string = tco.description,
+		morphed_obj: TCO.TestCaseObject = tco,
+		morphed: {morphdesc: string, morphedobject: TCO.TestCaseObject}[]
+	return morphed
 }
